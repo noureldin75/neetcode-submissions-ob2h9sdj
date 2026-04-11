4c9f48350325bdbe272c7f5bdfe1128c003e6cd7
NourEldeen Mohamed <139542786+noureldin75@users.noreply.github.com>
2026-04-11 23:41:46 +0200

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int Freq[26]={0};
+        int l=0;
+        int Maxfreq=0;
+        int Maxcount=0;
+        for(int r=0;r<s.size();r++){
+            int idx=s[r]-'A';
+            Freq[idx]++;
+            Maxfreq=max(Maxfreq,Freq[idx]);
+            if(r-l+1-k>Maxfreq){
+                Freq[s[l]-'A']--;
+                l++;
+            }
+            Maxcount=max(r-l+1,Maxcount);
+        }
+         for(auto it:Freq){
+            cout<< it;
+           }
+           return Maxcount;
+          
+        
+    }
+};
